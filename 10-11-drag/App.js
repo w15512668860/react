cc3b2d1a86349677702925f6d776730d3cd3fcfa
安歌 <53222789+w15512668860@users.noreply.github.com>
2019-11-04 15:55:22 +0800

    拖拽

---
+++ 10-11-drag/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Drag from './components/drag/Drag'
+import './App.css'
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Drag />
+            </div>
+        )
+    }
+}
